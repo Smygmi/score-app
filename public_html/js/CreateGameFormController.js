13196c675569e66125cf0e56f616c60cbcaa52ad
vherne <vherne@gmail.com>
2016-07-23 16:08:30 +0300

    format score fields to player1 and 2
    - split score field to individual players

--- public_html/js/CreateGameFormController.js
+++ public_html/js/CreateGameFormController.js
@@ -8,15 +8,17 @@ module.controller('CreateGameFormController', ['$scope', '$http', function ($sco
 
             $http.get("/getListOfOtherUsers", {headers: {'Accestoken': getAccess_Token()}}).then(function (users) {
 
-                        $scope.users = users.data.response.data;
-                        $http.get("/me", {headers: {'Accestoken': getAccess_Token()}}).then(function (user) {
-                                    $scope.users.push(user.data.response.data[0]);
-                                });
-                    });
+                $scope.users = users.data.response.data;
+                $http.get("/me", {headers: {'Accestoken': getAccess_Token()}}).then(function (user) {
+                    $scope.users.push(user.data.response.data[0]);
+                });
+            });
 
             $scope.Player1;
             $scope.Player2;
             $scope.Score;
+            $scope.Score1 = 0;
+            $scope.Score2 = 0;
 
             $scope.gameRoom = {
                 data: {
@@ -34,13 +36,13 @@ module.controller('CreateGameFormController', ['$scope', '$http', function ($sco
                 delete $scope.gameRoom.data.Player1UserName;
                 delete $scope.gameRoom.data.Player2;
                 delete $scope.gameRoom.data.Player2UserName;
-                
+
                 //Checks if Title is empty
                 if ($scope.gameRoom.Title === "") {
                     //$scope.NoTitleError.style.display = 'initial';
                     return;
                 } else
-                    //$scope.NoTitleError.style.display = 'none';
+                //$scope.NoTitleError.style.display = 'none';
 
                 //Checks if Starting Date is empty
                 if ($scope.gameRoom.StartingDate === "")
@@ -48,7 +50,7 @@ module.controller('CreateGameFormController', ['$scope', '$http', function ($sco
                     //$scope.NoStartingDateError.style.display = 'initial';
                     return;
                 } else
-                    //$scope.NoStartingDateError.style.display = 'none';
+                //$scope.NoStartingDateError.style.display = 'none';
 
                 //Checks if Ending Date is empty
                 if ($scope.gameRoom.EndingDate === "")
@@ -56,7 +58,7 @@ module.controller('CreateGameFormController', ['$scope', '$http', function ($sco
                     //$scope.NoEndingDateError.style.display = 'initial';
                     return;
                 } else
-                    //$scope.NoEndingDateError.style.display = 'none';
+                //$scope.NoEndingDateError.style.display = 'none';
 
 
                 /*//Checks if Ending Date is set earlier than starting date
@@ -79,7 +81,7 @@ module.controller('CreateGameFormController', ['$scope', '$http', function ($sco
                         }
                     }
                 }
-                
+
                 if ($scope.Player2)
                 {
                     $scope.gameRoom.data.Player2 = $scope.Player2;
@@ -93,14 +95,16 @@ module.controller('CreateGameFormController', ['$scope', '$http', function ($sco
                         }
                     }
                 }
+                $scope.gameRoom.data.Score = "" + $scope.Score1 + "-" + $scope.Score2;
+
                 var toPush = JSON.stringify($scope.gameRoom);
 
                 console.log(toPush);
 
                 $http.post("/createGameRoom", toPush, {headers: {'Accestoken': getAccess_Token()}}).then(function (response)
-                        {
-                            console.log(response);
-                        });
+                {
+                    console.log(response);
+                });
             };
         }
     }]);
